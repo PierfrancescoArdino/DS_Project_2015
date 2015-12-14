2f0f18b1a285fdc5a3f3e1ab208e02ee63dd9675
Alex De Biasio <alexdebiasio@gmail.com>
2015-12-14 12:08:59 +0100

    start build bank network

---
+++ bank.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import socket
+import threading
+from bankinterfaceout import bankInterfaceOut
+import time
+
+class Bank:
+    
+
+    def __init__(self, start_port, bank_number, total_banks):
+        self.bank_interface_out_list = []
+        threading.Thread(target = self.setup_server, 
+                                args = (start_port + bank_number,)).start()
+        time.sleep(2)
+        for i in xrange(0, total_banks):
+            if i != bank_number:
+                self.bank_interface_out_list.append(bankInterfaceOut(
+                                        start_port+i, i, bank_number))
+
+
+    def setup_server(self, port):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #server_socket.bind((socket.gethostname(), port))
+        server_socket.bind(('', port))
+        print "bind " + str( port)
+        server_socket.listen(10)
+
+        while True:
+            (client_sock, addr) = server_socket.accept()
+            bank_id = client_sock.recv(16)
+            print "first receiv " +bank_id
+            threading.Thread( target = self.recv_handl,
+                                args = (client_sock, bank_id)).start()
+
+    def recv_handl(self, client_sock, bank_id):
+        print "bank id: " + bank_id
+        while True:
+            tmp = client_sock.recv(16)
+            print "received" +tmp
+
---
+++ bankinterfaceout.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import socket
+
+class bankInterfaceOut:
+
+    def __init__(self, bank_port, bank_id, my_id):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #FIXME add host param
+        print "i'm bank " + str(my_id) + " try to connect to " + str(bank_port)
+        self.sock.connect(("127.0.0.1", bank_port))
+        self.bank_id = bank_id
+        self.sock.send(str(my_id))
+
