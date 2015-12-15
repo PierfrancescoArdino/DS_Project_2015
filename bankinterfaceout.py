e506f5190e192c5f7436369e8287ce1100e68fe7
Pierfrancesco Ardino <pierfrancesco.ardino@studenti.unitn.it>
2015-12-15 10:42:27 +0000

    fix connection

--- bankinterfaceout.py
+++ bankinterfaceout.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python2.7
 import socket
-
+import errno
+import time
+from socket import error as socket_error
 class bankInterfaceOut:
-
+    #connected = False
     def __init__(self, bank_port, bank_id, my_id):
+        connected = False
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #FIXME add host param
-        print "i'm bank " + str(my_id) + " try to connect to " + str(bank_port)
-        self.sock.connect(("127.0.0.1", bank_port))
+        while (connected is not True):
+            try:
+                print "i'm bank " + str(my_id) + " try to connect to " + str(bank_port)
+                self.sock.connect(("127.0.0.1", bank_port))
+                connected = True
+            except socket_error as serr:
+                print "somethings gone wrong, i'm trying to connect again in 2 seconds"
+                time.sleep(2)
         self.bank_id = bank_id
         self.sock.send(str(my_id))
 
