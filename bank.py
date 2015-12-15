57ca9e1ba2649f2f0e4b6b1651758dfa6a6b5561
Pierfrancesco Ardino <pierfrancesco.ardino@studenti.unitn.it>
2015-12-15 09:43:49 +0000

    add prints for debugging

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import socket
 import threading
@@ -6,11 +6,12 @@ from bankinterfaceout import bankInterfaceOut
 import time
 
 class Bank:
-    
+
 
     def __init__(self, start_port, bank_number, total_banks):
+        self.bank_number = bank_number
         self.bank_interface_out_list = []
-        threading.Thread(target = self.setup_server, 
+        threading.Thread(target = self.setup_server,
                                 args = (start_port + bank_number,)).start()
         time.sleep(2)
         for i in xrange(0, total_banks):
@@ -23,13 +24,12 @@ class Bank:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #server_socket.bind((socket.gethostname(), port))
         server_socket.bind(('', port))
-        print "bind " + str( port)
         server_socket.listen(10)
 
         while True:
             (client_sock, addr) = server_socket.accept()
             bank_id = client_sock.recv(16)
-            print "first receiv " +bank_id
+            print "i'm bank " + str(self.bank_number) + " received from " + str(bank_id)
             threading.Thread( target = self.recv_handl,
                                 args = (client_sock, bank_id)).start()
 
@@ -37,5 +37,6 @@ class Bank:
         print "bank id: " + bank_id
         while True:
             tmp = client_sock.recv(16)
-            print "received" +tmp
+            if tmp is not None:
+                print "received" + tmp
 
--- start_banks.py
+++ start_banks.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 from bank import Bank
 import subprocess
 
-START_PORT = 10000
+START_PORT = 9000
 NUMBER_OF_BANKS = 4
 
 
 for i in xrange(0, NUMBER_OF_BANKS):
     subprocess.Popen(["./bank_main.py",str(START_PORT),str(i),
                                     str(NUMBER_OF_BANKS)])
-    
+
